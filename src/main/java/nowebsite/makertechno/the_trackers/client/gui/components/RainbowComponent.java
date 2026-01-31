af0306740a1c71a8805b0a13a2364b8caa60edee
westernat <westernat233@163.com>
2026-02-01 00:36:21 +0800

    修一些问题

--- src/main/java/nowebsite/makertechno/the_trackers/client/gui/components/RainbowComponent.java
+++ src/main/java/nowebsite/makertechno/the_trackers/client/gui/components/RainbowComponent.java
@@ -2,9 +2,6 @@ package nowebsite.makertechno.the_trackers.client.gui.components;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.GuiGraphics;
-import org.jetbrains.annotations.NotNull;
-
-import java.awt.*;
 
 public class RainbowComponent extends BaseComponent {
     private final int intervalColor;
@@ -19,7 +16,7 @@ public class RainbowComponent extends BaseComponent {
         return intervalColor > 1;
     }
     @Override
-    public void render(@NotNull GuiGraphics graphics, float partialTick) {
+    public void render(GuiGraphics graphics, float partialTick) {
         if (++intervalCount >= intervalColor) {
             osc();
             intervalCount = 0;
@@ -31,10 +28,59 @@ public class RainbowComponent extends BaseComponent {
     private void osc() {
         hue += 0.01F;
         if (hue > 1f) hue -= 1f;
-        int rgb = Color.HSBtoRGB(hue, 1f, 1f);
+        int rgb = HSBtoRGB(hue, 1f, 1f);
 
         r = ((rgb >> 16) & 0xFF) / 255f;
         g = ((rgb >> 8) & 0xFF) / 255f;
         b = (rgb & 0xFF) / 255f;
+    }
+
+    /// 有些Java构建版本没有awt
+    private static int HSBtoRGB(float hue, float saturation, float brightness) {
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        if (saturation == 0.0F) {
+            r = g = b = (int)(brightness * 255.0F + 0.5F);
+        } else {
+            float h = (hue - (float)Math.floor(hue)) * 6.0F;
+            float f = h - (float)Math.floor(h);
+            float p = brightness * (1.0F - saturation);
+            float q = brightness * (1.0F - saturation * f);
+            float t = brightness * (1.0F - saturation * (1.0F - f));
+            switch ((int)h) {
+                case 0:
+                    r = (int)(brightness * 255.0F + 0.5F);
+                    g = (int)(t * 255.0F + 0.5F);
+                    b = (int)(p * 255.0F + 0.5F);
+                    break;
+                case 1:
+                    r = (int)(q * 255.0F + 0.5F);
+                    g = (int)(brightness * 255.0F + 0.5F);
+                    b = (int)(p * 255.0F + 0.5F);
+                    break;
+                case 2:
+                    r = (int)(p * 255.0F + 0.5F);
+                    g = (int)(brightness * 255.0F + 0.5F);
+                    b = (int)(t * 255.0F + 0.5F);
+                    break;
+                case 3:
+                    r = (int)(p * 255.0F + 0.5F);
+                    g = (int)(q * 255.0F + 0.5F);
+                    b = (int)(brightness * 255.0F + 0.5F);
+                    break;
+                case 4:
+                    r = (int)(t * 255.0F + 0.5F);
+                    g = (int)(p * 255.0F + 0.5F);
+                    b = (int)(brightness * 255.0F + 0.5F);
+                    break;
+                case 5:
+                    r = (int)(brightness * 255.0F + 0.5F);
+                    g = (int)(p * 255.0F + 0.5F);
+                    b = (int)(q * 255.0F + 0.5F);
+            }
+        }
+
+        return -16777216 | r << 16 | g << 8 | b;
     }
 }
